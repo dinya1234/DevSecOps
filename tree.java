e0e2820479035d2243af55bb5562971d5387d78e
дыня <ulianagaladzeva@gmail.com>
2025-10-29 13:33:10 +0300

    v1.4.2
    добавлена звезда на верхушку елки
    на нижнем ярусе добавлена гирлянда 
    под ёлкой добавлены подарки

--- tree.java
+++ tree.java
@@ -4,14 +4,25 @@ public class Main {
         String reset = "\u001B[0m";
         String green = "\u001B[32m";
         String brown = "\u001B[33m";
+        String red = "\u001B[31m";
+        String yellow = "\u001B[93m";
+        String blue = "\u001B[34m";
         
+        System.out.println(yellow + "     *    " + reset);
         System.out.println(green + "     *    " + reset);
         System.out.println(green + "    ***   " + reset);
-        System.out.println(green + "   *****  " + reset);
-        System.out.println(brown + "    | |   " + reset);
+        System.out.println(green + "   *" + red + "°" + green + "*" + blue + "°" + green + "*  " + reset);
+
+        char box = '▣'; 
+
+        System.out.print(red + box + box + " " + reset);
+        System.out.print(brown + "| |" + reset);
+        System.out.print(blue + box  + reset);
+        System.out.println();
+
         for (int i = 0; i < 3; i++) {
             System.out.println("MERRY CHRISTMAS!");
         }
+        
     }
 }
-
