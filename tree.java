b6078b3ce8195c69e7bd95e8e7f21c6a379987be
дыня <ulianagaladzeva@gmail.com>
2025-10-29 13:16:18 +0300

    v1.3.2
    исправлен баг с тем что один ярус елки не изменил цвет

--- tree.java
+++ tree.java
@@ -5,8 +5,8 @@ public class Main {
         String green = "\u001B[32m";
         String brown = "\u001B[33m";
         
-        System.out.println(green + "     *    ");
-        System.out.println("    ***   " + reset); 
+        System.out.println(green + "     *    " + reset);
+        System.out.println(green + "    ***   " + reset);
         System.out.println(green + "   *****  " + reset);
         System.out.println(brown + "    | |   " + reset);
         for (int i = 0; i < 3; i++) {
